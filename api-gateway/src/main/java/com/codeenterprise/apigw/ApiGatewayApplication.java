2239ddcbd8b5d2eb0f411b1e184c6247431a3f4d
taskeenh <thaider@stellatechnology.com>
2022-04-01 02:37:58 +0500

    added jib for dockrization

--- api-gateway/src/main/java/com/codeenterprise/apigw/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codeenterprise/apigw/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.codeenterprise.apigw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaClient
